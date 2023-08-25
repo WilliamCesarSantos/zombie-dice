7d083944c3e129ead130067f69106297592a8d1e
William Cesar Santos <william_cesar_santos@hotmail.com>
2023-08-25 20:56:48 -0300

    Zombie

--- src/br/btg/zombiedice/Main.java
+++ src/br/btg/zombiedice/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Die die = new RedDie();
-        System.out.println("O valor sorteado foi: "+die.roll());
+        System.out.println("O valor sorteado foi: " + die.roll());
     }
 
 }

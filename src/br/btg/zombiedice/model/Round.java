70c3bdea9d53f71f6f15e0c8e6135efad6cfc164
William Cesar Santos <william_cesar_santos@hotmail.com>
2023-08-28 21:21:49 -0300

    Zombie

--- src/br/btg/zombiedice/model/Round.java
+++ src/br/btg/zombiedice/model/Round.java
@@ -2,13 +2,14 @@ package br.btg.zombiedice.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Representação do turno
  */
 public class Round {
 
-    private List<Die> dices = new ArrayList<>();
+    private List<Pair<Die, DieValue>> dices = new ArrayList<>();
     private List<Die> runners = new ArrayList<>();
     private Zombie zombie;
 
@@ -18,8 +19,9 @@ public class Round {
 
     public void play(Bowl bowl) {
         List<Pair<Die, DieValue>> diceRolled = zombie.play(bowl, runners);
+        runners.clear();
         for (Pair<Die, DieValue> dieRolled : diceRolled) {
-            dices.add(dieRolled.getKey());
+            dices.add(dieRolled);
             if (dieRolled.getValue() == DieValue.BRAIN) {
                 zombie.gotBrain();
             } else if (dieRolled.getValue() == DieValue.SHOOT) {
@@ -28,9 +30,45 @@ public class Round {
                 runners.add(dieRolled.getKey());
             }
         }
-        if (zombie.getShootCount() < 3) {
+        if (keepPlaying()) {
             play(bowl);
+        } else {
+            zombie.consolidate(this);
         }
+    }
+
+    /**
+     * A quantidade de cerebros consumidas nesse turno
+     * @return
+     */
+    public Integer getBrainAccount(){
+        Integer counter = 0;
+        for (Pair<Die, DieValue> dieRolled : dices) {
+            if (dieRolled.getValue() == DieValue.BRAIN) {
+                counter += 1;
+            }
+        }
+        return counter;
+    }
+
+    private Boolean keepPlaying() {
+        Boolean keepPlaying = true;
+        if (zombie.getBrainCount() >= 13) {
+            System.out.println(zombie.getName() + " parabens, você foi o vencedor!");
+            keepPlaying = false;
+        } else if (zombie.getShootCount() >= 3) {
+            System.out.println(zombie.getName() + ", você perdeu esse turno!");
+            keepPlaying = false;
+        } else {
+            System.out.println("Nesse round você levou " +
+                    zombie.getShootCount() + " tiros e consumiu " +
+                    zombie.getBrainCount() + " cerebros.");
+            System.out.println("Deseja jogar novamente(s/n)?");
+            Scanner scanner = new Scanner(System.in);
+            String userOption = scanner.nextLine();
+            keepPlaying = userOption.equalsIgnoreCase("s");
+        }
+        return keepPlaying;
     }
 
 }
--- src/br/btg/zombiedice/model/Zombie.java
+++ src/br/btg/zombiedice/model/Zombie.java
@@ -53,4 +53,8 @@ public class Zombie {
         return used;
     }
 
+    public void consolidate(Round round) {
+
+    }
+
 }
